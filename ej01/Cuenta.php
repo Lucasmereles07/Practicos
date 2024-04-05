07505a2a1b2bef6d21df82422fbfc27acf7f488d
Lucas Mereles <lucasmereles29@gmail.com>
2024-04-05 13:18:14 -0300

    Agregue ejercicio 1

---
+++ ej01/Cuenta.php
@@ -0,0 +1,13 @@
+<?php 
+class Cuenta {
+    private $cuenta;
+
+    /**
+     * Constructor.
+     * @param $cuenta
+     */
+    public function __construct($cuenta)
+    {
+        $this->cuenta = $cuenta;
+    }
+}
---
+++ ej01/principal.php
@@ -0,0 +1,5 @@
+<?php 
+require_once 'ej01/Cuenta.php';
+$cuenta = new Cuenta("hola");
+var_dump($cuenta);
+; ?>
